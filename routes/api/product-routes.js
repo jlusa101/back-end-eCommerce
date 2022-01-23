92c80b6b16c9821665f858c1f841ffd8aa3ec5c6
Joona Lusa <joona.lusa@hotmail.com>
2022-01-23 08:32:38 -0500

    added all CRUD methods to the product model

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,24 +47,23 @@ router.get('/:id', (req, res) => {
         })
 });
 
-// create new product
+// A route that creates a new product in the database
 router.post('/', (req, res) => {
-    /* req.body should look like this...
-      {
-        product_name: "Basketball",
-        price: 200.00,
-        stock: 3,
-        tagIds: [1, 2, 3, 4]
-      }
-    */
-    Product.create(req.body)
+    Product.create({
+            product_name: req.body.product_name,
+            price: req.body.price,
+            stock: req.body.stock,
+            category_id: req.body.category_id,
+            tagIds: req.body.tagIds
+        })
         .then((product) => {
             // if there's product tags, we need to create pairings to bulk create in the ProductTag model
             if (req.body.tagIds.length) {
+                console.log(req.body.tagIds);
                 const productTagIdArr = req.body.tagIds.map((tag_id) => {
                     return {
                         product_id: product.id,
-                        tag_id,
+                        tag_id: tag_id,
                     };
                 });
                 return ProductTag.bulkCreate(productTagIdArr);
@@ -79,10 +78,14 @@ router.post('/', (req, res) => {
         });
 });
 
-// update product
+// A route that updates a product's attributes based on its ID
 router.put('/:id', (req, res) => {
-    // update product data
     Product.update(req.body, {
+            product_name: req.body.product_name,
+            price: req.body.price,
+            stock: req.body.stock,
+            category_id: req.body.category_id,
+            tagIds: req.body.tagIds,
             where: {
                 id: req.params.id,
             },
@@ -121,8 +124,24 @@ router.put('/:id', (req, res) => {
         });
 });
 
+// A route that deletes a product from the database based on its ID
 router.delete('/:id', (req, res) => {
-    // delete one product by its `id` value
+    Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(tag => {
+            if (!tag) {
+                res.status(404).json({ message: 'No product found with this id' });
+                return;
+            }
+            res.json(tag);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 module.exports = router;
