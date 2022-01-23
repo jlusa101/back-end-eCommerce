7fd3abf24c8bc37a37dc1312330df4fc49f9e7b3
Joona Lusa <joona.lusa@hotmail.com>
2022-01-22 19:31:30 -0500

    added CRUD routes for the tags

--- models/index.js
+++ models/index.js
@@ -24,7 +24,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
     through: ProductTag,
-    as: 'tag',
+    as: 'product',
     foreignKey: 'tag_id'
 })
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,95 @@
+// Required dependancies
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
+// A route that retrieves all the tags from the database
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+    Tag.findAll({
+            include: [{
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+                through: ProductTag,
+                as: 'product'
+            }]
+        }).then(tags => res.json(tags))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
+// A route that retrieves a tag based on its ID from the database
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    Tag.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+                through: ProductTag,
+                as: 'product'
+            }]
+        }).then(tag => res.json(tag))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
+// A route that adds a new tag to the database
 router.post('/', (req, res) => {
-  // create a new tag
+    Tag.create({
+            tag_name: req.body.tag_name
+        })
+        .then(tag => {
+            res.json(tag)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
+// A route that updates a tag's name based on its ID in the database
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    Tag.update({
+            tag_name: req.body.tag_name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(tag => {
+            if (!tag[0]) {
+                res.status(404).json({ message: 'No tag found with this id' });
+                return;
+            }
+            res.json(tag);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
+// A route that deletes a tag from the database based on its ID
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    Tag.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(tag => {
+            if (!tag) {
+                res.status(404).json({ message: 'No tag found with this id' });
+                return;
+            }
+            res.json(tag);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 module.exports = router;
